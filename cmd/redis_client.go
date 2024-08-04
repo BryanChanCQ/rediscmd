8caeaaeac80c9cc41540e3a2bd13712bd3e2cc6c
zzm996-zzm <1416601780@qq.com>
2024-08-04 13:59:02 +0800

    feat:检测redis是否连接成功

--- cmd/redis_client.go
+++ cmd/redis_client.go
@@ -1,9 +1,28 @@
 package cmd
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func CreateRedisConnection() redis.Cmdable {
-	return redis.NewClient(&redis.Options{
+	client := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
+
+	// 检查连接
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error connecting to Redis:%v\n", err))
+	}
+
+	// fmt.Printf("Successfully connected to Redis. Response: %s\n", pingResp)
+
+	return client
+
 }
