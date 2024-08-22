cfb8dc70cfe58627e50add08fc4940500ab2a008
bryanchan <363256394@qq.com>
2024-08-22 22:08:16 +0800

    fix 根据shellcheck的建议修改脚本

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
-os=$1
-platform=$2
-echo $os
-echo $platform
-if [ $os == 'linux' ]; then
+#!/bin/bash
+os="$1"
+platform="$2"
+echo "$os"
+echo "$platform"
+if [ "$os" = 'linux' ]; then
     GOOS=linux GOARCH=amd64 go build -o ./bin/redisCmd ./main.go
 fi
 
-if [ $os == 'macos' ]; then
+if [ "$os" = 'macos' ]; then
     GOOS=darwin GOARCH=amd64 go build -o ./bin/redisCmd ./main.go
 fi
 
-if [[ $os == 'macos' && $platform == 'arm' ]]; then
+if [ "$os" = 'macos' ] && [ "$platform" = 'arm' ]; then
     GOOS=darwin GOARCH=arm64 go build -o ./bin/redisCmd ./main.go
 fi
 
-if [ $os == 'windows' ]; then
+if [ "$os" = 'windows' ]; then
     GOOS=windows GOARCH=amd64 go build -o ./bin/redisCmd.exe ./main.go
 fi
